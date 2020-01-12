153389c5f4e9c8f317102f0d098dd0c5676c4d4f
satya-vinay <satyavinay2@gmail.com>
2020-01-12 08:02:18 -0500

    plugins

--- install.sh
+++ install.sh
@@ -3,3 +3,15 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 #change default Shell to zsh
 chsh -s $(which zsh)
+
+#Install plugins to oh-my-zsh shell
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+#Install shell terminal multiplexer
+sudo apt install tmux -y
+
+#Install fonts
+sudo apt install powerline -y
