cccc88a50165a6e2881f81aca9a3c4e38e41f9f8
satya-vinay <satyavinay2@gmail.com>
2020-07-31 13:59:22 -0400

    vs code install

--- install.sh
+++ install.sh
@@ -5,6 +5,15 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 
 
+sudo apt install software-properties-common apt-transport-https wget
+
+
+#Install VS-CODE editor
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+sudo apt install code 
 
 sudo apt-get install -y python3-pip
 
